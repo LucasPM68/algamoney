66b039927c2f6936d2399e8bf22e87ddfeaf5494
LucasPM68 <lucasperamarchi@hotmail.com>
2019-07-01 17:27:46 -0300

    adicionado autenticacao

---
+++ algamoney-api/src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
@@ -0,0 +1,34 @@
+package com.example.algamoney.api.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+	
+	@Autowired
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ROLE");
+	}
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/categorias").permitAll() //categorias todos podem acessar
+		.anyRequest().authenticated() //qualquer requisição (exceto categorias) tem q estar autenticadas	
+		.and()
+		.httpBasic().and()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()//n iremos usar sessions
+		.csrf().disable(); //n permite javascript injection
+	}
+
+}
